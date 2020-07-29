cc0b09f1d6019f0ec72953b557f8c46349f65f5c
praguna <praguna.manvi@dailyhunt.in>
2020-07-29 17:26:43 +0530

    duplicated syntax analyze,added pojo classes

---
+++ compiler/src/main/java/SymbolTable.java
@@ -0,0 +1,24 @@
+enum SymbolType{
+    STATIC,FIELD,ARG,VAR
+}
+
+public class SymbolTable {
+    SymbolTable(){
+
+    }
+    void define(){
+
+    }
+    int varCount(SymbolType type){
+        return 0;
+    }
+    SymbolType kindOf(String name){
+        return null;
+    }
+    String typeOf(String name){
+        return null;
+    }
+    int indexOf(String name){
+        return 0;
+    }
+}
---
+++ compiler/src/main/java/VMWriter.java
@@ -0,0 +1,35 @@
+public class VMWriter {
+    VMWriter(){
+
+    }
+    void writePush(String segment){
+
+    }
+    void writePop(String segment){
+
+    }
+    void writeArithmetic(String command){
+
+    }
+    void writeLabel(String label){
+
+    }
+    void writeGoto(String label){
+
+    }
+    void writeIf(String label){
+
+    }
+    void writeCall(String name, int nArgs){
+
+    }
+    void writeFunction(String name, int nLocals){
+
+    }
+    void writeReturn(){
+
+    }
+    void close(){
+
+    }
+}
